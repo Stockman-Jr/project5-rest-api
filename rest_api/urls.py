c78f442e11624b4cc84999e97e13ba74084473eb
Stockman-Jr <stockmanmimmi@gmail.com>
2023-02-11 18:23:26 +0000

    Add serializers and views for comments

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
     path('', include('likes.urls')),
-    #path('', include('comments.urls')),
+    path('', include('comments.urls')),
 ]
