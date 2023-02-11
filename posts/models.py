594ebf0e70da5177289d3e9f54be019401fc425f
Stockman-Jr <stockmanmimmi@gmail.com>
2023-02-11 17:07:44 +0000

    Fix models in posts app by restructuring

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from multiselectfield import MultiSelectField
 from django.contrib.auth.models import User
 from pokemons.models import CaughtPokemon, Nature, HeldItem
+from model_utils.managers import InheritanceManager
 
 
 GAME_CHOICES = [
@@ -28,17 +29,25 @@ EV_CHOICE_STATS = [
     ]
 
 
-class Post(models.Model):
+class BasePost(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/', blank=True)
-    ingame_name = models.CharField(max_length=255, blank=True)
+    post_type = models.CharField(max_length=100)
     game_filter = models.CharField(
         max_length=32, choices=GAME_CHOICES, default=''
     )
+    objects = InheritanceManager()
+
+    def __str__(self):
+        return f"{self.post_type} {self.id}"
+
+
+class Post(BasePost):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/', blank=True)
+    ingame_name = models.CharField(max_length=255, blank=True)
 
     class Meta:
         ordering = ['-created_at']
@@ -47,10 +56,7 @@ class Post(models.Model):
         return f"{self.id} {self.title}"
 
 
-class PokemonBuildPost(models.Model):
-    owner = models.ForeignKey(
-        User, on_delete=models.CASCADE, null=True, blank=True
-        )
+class PokemonBuild(BasePost):
     pokemon = models.ForeignKey(
         CaughtPokemon, related_name='pokemons_caught', on_delete=models.CASCADE
         )
@@ -63,11 +69,6 @@ class PokemonBuildPost(models.Model):
     held_item = models.ForeignKey(HeldItem, on_delete=models.CASCADE)
     ev_stats = MultiSelectField(
         max_length=100, max_choices=2, choices=EV_CHOICE_STATS, default=''
-    )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    game_filter = models.CharField(
-        max_length=32, choices=GAME_CHOICES, default=''
     )
 
     class Meta:
