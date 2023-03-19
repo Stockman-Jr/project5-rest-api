493625b3c79253873114a40dcb5fdbf0765e6fa1
Stockman-Jr <stockmanmimmi@gmail.com>
2023-03-19 14:52:55 +0000

    Change pagination class

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -34,7 +34,7 @@ REST_FRAMEWORK = {
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
     'DEFAULT_PAGINATION_CLASS':
-        'rest_framework.pagination.PageNumberPagination',
+        'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
     'DATETIME_FORMAT': '%d %b %Y',
 }
